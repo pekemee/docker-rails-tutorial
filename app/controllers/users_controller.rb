c6fcfae3b76d9906f5790164324411e19085ac49
pekemee <riyoniriverplus2@yahoo.co.jp>
2020-06-02 10:29:31 +0900

    fin 10th delete&index user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :adimin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
     @user = User.new
   end
@@ -18,8 +26,44 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "プロフィールは更新されました"
+      redirect_to @user
+    else
+      render ''
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    flash[:success] = "#{@user.name}を削除しました"
+    @user.destroy
+    redirect_to users_url
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+  #before_action
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def adimin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
 end
