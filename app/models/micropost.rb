7b86cacac5a529db9c863b5cb0a66f27744634a5
pekemee <riyoniriverplus2@yahoo.co.jp>
2020-06-03 15:57:32 +0900

    fin 14th without prod_env

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -8,7 +8,6 @@ class Micropost < ApplicationRecord
 
   private
 
-    # アップロードされた画像のサイズをバリデーションする
     def picture_size
       if picture.size > 5.megabytes
         errors.add(:picture, "should be less than 5MB")
