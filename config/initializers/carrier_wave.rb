b5cb7a2f59566673e27766e5a429afc52e66f808
pekemee <riyoniriverplus2@yahoo.co.jp>
2020-06-03 22:48:16 +0900

    change s3 setting

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,10 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-
       :provider              => 'AWS',
-      :region                => 'ap-northeast-1',
-      :aws_access_key_id     => 'AKIA5B44PNDE4TL5I7OV',
-      :aws_secret_access_key => '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'
+      :region                => ENV['S3_REGION'],
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
     config.fog_directory     =  'docker-rails-tutorial-bucket'
   end
