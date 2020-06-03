baa60bbdc90e8bfbff115075c659804c4fde18ae
pekemee <riyoniriverplus2@yahoo.co.jp>
2020-06-03 22:22:40 +0900

    add prod_setting

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,12 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+
+      :provider              => 'AWS',
+      :region                => 'ap-northeast-1',
+      :aws_access_key_id     => 'AKIA5B44PNDE4TL5I7OV',
+      :aws_secret_access_key => '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'
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
+end
