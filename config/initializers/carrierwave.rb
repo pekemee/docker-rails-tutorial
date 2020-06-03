136fe9969c14c86a4c311d6f10654c02e70ba520
pekemee <riyoniriverplus2@yahoo.co.jp>
2020-06-03 23:54:55 +0900

    change carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,15 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      :provider              => 'AWS',
-      :region                => ENV['S3_REGION'],
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
-    }
-    config.fog_directory     =  'docker-rails-tutorial-bucket'
-  end
-end
+        config.fog_provider = 'fog/aws'
+        config.fog_credentials = {
+        # Configuration for Amazon S3
+        :provider              => 'AWS',
+        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+        :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+        :region                => ENV['S3_REGION'],
+  }
+    config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
+    config.fog_directory  = ENV['S3_BUCKET']
+    config.fog_public     = true
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
+   end
