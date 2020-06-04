3959a724f71c5d7efdf17358c01d4ea2ca4818cd
pekemee <riyoniriverplus2@yahoo.co.jp>
2020-06-04 10:59:58 +0900

    add rspec gem

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,20 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(*Rails.groups)  
+
+module Projects
+   class Application < Rails::Application
+     config.load_defaults 5.1
+     config.generators do |g|
+       g.test_framework :rspec,
+         fixtures: false,
+         view_specs: false,
+         helper_specs: false,
+         routing_specs: false
+     end
+   end
+end
 
 module AppName
   class Application < Rails::Application
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "nameがなければ無効"do
+    user=User.new(name:nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+end
