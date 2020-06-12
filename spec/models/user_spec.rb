6b13caa4631620623a4352bbf1947b5de73d9262
けん こもり <riyoniriverplus2@yahoo.co.jp>
2020-06-12 21:17:06 +0900

    add_test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe User, type: :model do
     user.valid?
     expect(user.errors[:name]).to include("can't be blank")
   end
+
+  it "nameがなければ無効"do
+    user=User.new(name:nil)
+    user.valid?
+    expect(user.errors[:name]).not_to include("can't be blank")
+  end
 end
