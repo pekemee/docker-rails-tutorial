fd299142202c7753a24af4fb1228f998ed277d47
けん こもり <riyoniriverplus2@yahoo.co.jp>
2020-06-12 21:20:29 +0900

    modify_model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,6 @@ RSpec.describe User, type: :model do
   it "nameがなければ無効"do
     user=User.new(name:nil)
     user.valid?
-    expect(user.errors[:name]).not_to include("can't be blank")
+    expect(user.errors[:name]).to include("can't be blank")
   end
 end
